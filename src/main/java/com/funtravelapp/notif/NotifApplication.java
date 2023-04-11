11123c86a58a8ab2d0a0d756f2fdebb1ab4aff4e
pratamafarhan10 <pratamafarhan10@gmail.com>
2023-04-11 15:41:42 +0700

    initialize

---
+++ src/main/java/com/funtravelapp/notif/NotifApplication.java
@@ -0,0 +1,13 @@
+package com.funtravelapp.notif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifApplication.class, args);
+	}
+
+}
